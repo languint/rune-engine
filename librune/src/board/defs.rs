b8b505de24e417c8301f23635e47b224ff67aaa0
languint <longuint@proton.me>
2025-11-13 06:49:49 -0600

    feat(lib): add `Square`

---
+++ librune/src/board/defs.rs
@@ -0,0 +1,49 @@
+/// A chess square using little endian mappings.
+/// 
+/// `0` -> `A1`
+/// `63` -> `H8`
+#[rustfmt::skip]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum Square {
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8,
+}
+
+/// All [`Squares`][`Square`]
+pub const ALL_SQUARES: [Square; 64] = {
+    let mut arr = [Square::A1; 64];
+    let mut i: u8 = 0;
+    while i < 64 {
+        arr[i as usize] = unsafe { std::mem::transmute::<u8, Square>(i) };
+        i += 1;
+    }
+    arr
+};
+
+impl TryFrom<u8> for Square {
+    type Error = String;
+    /// Attempt to create a [`Square`] from a `u8`
+    ///
+    /// # Errors
+    /// Returns `Err` if the value is greater than 63
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if value > 63 {
+            return Err(format!("`{value}` cannot be a square!"));
+        }
+
+        unsafe { Ok(std::mem::transmute::<u8, Square>(value)) }
+    }
+}
+
+impl From<Square> for u8 {
+    fn from(sq: Square) -> Self {
+        sq as u8
+    }
+}
