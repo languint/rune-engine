fd44b95ec1fa19e26443ac02976ed9b35df2e32a
languint <longuint@proton.me>
2025-11-13 06:51:59 -0600

    feat(lib): add `Rank`, `File`

--- librune/src/board/defs.rs
+++ librune/src/board/defs.rs
@@ -1,3 +1,129 @@
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Rank(pub u8);
+
+/// All [`Ranks`][`Rank`]
+pub const ALL_RANKS: [Rank; 8] = [
+    Rank(1),
+    Rank(2),
+    Rank(3),
+    Rank(4),
+    Rank(5),
+    Rank(6),
+    Rank(7),
+    Rank(8),
+];
+
+impl TryFrom<&char> for Rank {
+    type Error = String;
+
+    /// Attempt to construct a [`Rank`] from a `char`.
+    ///
+    /// Accepts digits `'1'` through `'8'`.
+    ///
+    /// # Errors
+    /// Returns `Err` if the rank couldn't be parsed.
+    fn try_from(value: &char) -> Result<Self, Self::Error> {
+        match value {
+            '1' => Ok(Rank(1)),
+            '2' => Ok(Rank(2)),
+            '3' => Ok(Rank(3)),
+            '4' => Ok(Rank(4)),
+            '5' => Ok(Rank(5)),
+            '6' => Ok(Rank(6)),
+            '7' => Ok(Rank(7)),
+            '8' => Ok(Rank(8)),
+            _ => Err(format!("Cannot parse `{value}` as a rank!")),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum File {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+}
+
+/// All [`Files`][`File`]
+pub const ALL_FILES: [File; 8] = [
+    File::A,
+    File::B,
+    File::C,
+    File::D,
+    File::E,
+    File::F,
+    File::G,
+    File::H,
+];
+
+impl TryFrom<&char> for File {
+    type Error = String;
+
+    fn try_from(value: &char) -> Result<Self, Self::Error> {
+        match value.to_ascii_lowercase() {
+            'a' => Ok(File::A),
+            'b' => Ok(File::B),
+            'c' => Ok(File::C),
+            'd' => Ok(File::D),
+            'e' => Ok(File::E),
+            'f' => Ok(File::F),
+            'g' => Ok(File::G),
+            'h' => Ok(File::H),
+            _ => Err(format!("Cannot parse `{value}` as a file!")),
+        }
+    }
+}
+
+impl TryFrom<u8> for File {
+    type Error = String;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if value > 7 {
+            return Err(format!("Cannot parse {value} as a file!"));
+        }
+        Ok(unsafe { std::mem::transmute::<u8, File>(value) })
+    }
+}
+
+impl From<File> for u8 {
+    fn from(file: File) -> Self {
+        file as u8
+    }
+}
+
+impl fmt::Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", (*self as u8 + b'a') as char)
+    }
+}
+
+impl File {
+    /// Transmute a `u8` as a [`File`]
+    ///
+    /// # Safety
+    /// This function is safe as long us `value` is less than `8`
+    #[must_use]
+    #[inline]
+    pub unsafe fn from_u8(value: u8) -> Self {
+        unsafe { std::mem::transmute(value) }
+    }
+
+    /// Get the `u8` representation of a [`File`]
+    #[must_use]
+    #[inline]
+    pub fn as_u8(self) -> u8 {
+        self as u8
+    }
+}
+
 /// A chess square using little endian mappings.
 /// 
 /// `0` -> `A1`
@@ -35,7 +161,7 @@ impl TryFrom<u8> for Square {
     /// Returns `Err` if the value is greater than 63
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         if value > 63 {
-            return Err(format!("`{value}` cannot be a square!"));
+            return Err(format!("Cannot parse `{value}` as a square!"));
         }
 
         unsafe { Ok(std::mem::transmute::<u8, Square>(value)) }
@@ -45,5 +171,29 @@ impl TryFrom<u8> for Square {
 impl From<Square> for u8 {
     fn from(sq: Square) -> Self {
         sq as u8
+    }
+}
+
+impl Square {
+    /// Get the [`File`] associated with this [`Square`]
+    #[inline]
+    #[must_use]
+    pub fn file(self) -> File {
+        unsafe { File::from_u8((self as u8) & 7) }
+    }
+
+    /// Get the [`Rank`] associated with this [`Square`]
+    #[inline]
+    #[must_use]
+    pub fn rank(self) -> Rank {
+        Rank(self as u8 >> 3)
+    }
+}
+
+impl fmt::Display for Square {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let file = (b'a' + u8::from(self.file())) as char;
+        let rank = (b'1' + self.rank().0) as char;
+        write!(f, "{file}{rank}")
     }
 }
