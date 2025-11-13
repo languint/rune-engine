e3a5793a769b1613e74b877bf32dda0fb8c4a237
languint <longuint@proton.me>
2025-11-13 07:29:38 -0600

    feat(lib): initialize `Board` struct

--- librune/src/board.rs
+++ librune/src/board.rs
@@ -1,3 +1,10 @@
+use crate::{board::bitboard::Bitboard, defs::NrOf};
+
 pub mod defs;
 pub mod pieces;
 pub mod bitboard;
+
+pub struct Board {
+    pub bb_pieces: [[Bitboard; NrOf::PIECE_TYPES as usize]; NrOf::SIDES as usize],
+    pub bb_occupancy: [Bitboard; NrOf::SIDES as usize],
+}
--- librune/src/defs.rs
+++ librune/src/defs.rs
@@ -1,6 +1,17 @@
+/// Sides of a chessboard, `WHITE`, `BLACK`, or `BOTH`
+pub struct Sides {}
+impl Sides {
+    pub const WHITE: u8 = 0;
+    pub const BLACK: u8 = 1;
+    pub const BOTH: u8 = 2;
+}
+
 pub struct NrOf {}
 impl NrOf {
     pub const FILES: u8 = 8;
     pub const RANKS: u8 = 8;
     pub const SQUARES: u8 = 64;
+
+    pub const PIECE_TYPES: u8 = 6;
+    pub const SIDES: u8 = 2;
 }
